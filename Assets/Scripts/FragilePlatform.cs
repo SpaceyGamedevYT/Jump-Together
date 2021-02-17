21023476404292ee99b836b7bdf445ea3bc45cc9
SpaceyGamedevYT <52182168+SpaceyGamedevYT@users.noreply.github.com>
2021-02-17 09:29:26 -0800

    added level restarting

---
+++ Assets/Scripts/FragilePlatform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FragilePlatform : MonoBehaviour
+{
+    int collisionCount = 0;
+    bool hasTouchedPlatform;
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        collisionCount++;
+        hasTouchedPlatform = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        collisionCount--;
+    }
+
+    private void Update() 
+    {
+        if(collisionCount <= 0 && hasTouchedPlatform)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/LevelRestarter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelRestarter : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
